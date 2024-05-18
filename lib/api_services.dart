575a1231837294b27d19ebec94667f52b34d5713
Saicharangattu02 <saicharan.teamup@gmail.com>
2024-05-18 11:10:42 +0530

    quizApi

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-var link = "https://opentdb.com/api.php?amount=20";
+var link = "https://www.teamup.ink/varadhi_backend/app_apis/get_test_paper.php";
 
 getQuiz() async {
   var res = await http.get(Uri.parse(link));
   if (res.statusCode == 200) {
     var data = jsonDecode(res.body.toString());
     print("data is loaded");
+    print("hii");
     return data;
   }
 }
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 
 import 'package:demo_application/api_services.dart';
-import 'package:demo_application/const/colors.dart';
-import 'package:demo_application/const/images.dart';
-import 'package:demo_application/const/text_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,11 +13,11 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
   var currentQuestionIndex = 0;
-  int seconds = 60;
+  int seconds = 30;
+  var data;
+
   Timer? timer;
   late Future quiz;
-
-  int points = 0;
 
   var isLoaded = false;
 
@@ -74,7 +71,7 @@ class _QuizScreenState extends State<QuizScreen> {
     currentQuestionIndex++;
     resetColors();
     timer!.cancel();
-    seconds = 60;
+    seconds = 30;
     startTimer();
   }
 
@@ -83,145 +80,170 @@ class _QuizScreenState extends State<QuizScreen> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: const EdgeInsets.all(12),
-        decoration: const BoxDecoration(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.all(12),
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [blue, darkBlue],
-        )),
-        child: FutureBuilder(
-          future: quiz,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              var data = snapshot.data["results"];
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.blueGrey],
+            ),
+          ),
+          child: 
+          FutureBuilder(
+            future: quiz,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                data = snapshot.data; // Assigning snapshot.data to data variable
 
-              if (isLoaded == false) {
-                optionsList = data[currentQuestionIndex]["incorrect_answers"];
-                optionsList.add(data[currentQuestionIndex]["correct_answer"]);
-                optionsList.shuffle();
-                isLoaded = true;
-              }
+                if (isLoaded == false) {
+                  optionsList = [
+                    data[currentQuestionIndex]["option1"],
+                    data[currentQuestionIndex]["option2"],
+                    data[currentQuestionIndex]["option3"],
+                    data[currentQuestionIndex]["option4"],
+                    data[currentQuestionIndex]["correct_answer"],
+                  ];
+                  optionsList.shuffle();
+                  isLoaded = true;
+                }
 
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(color: lightgrey, width: 2),
-                          ),
-                          child: IconButton(
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(color: Colors.grey, width: 2),
+                            ),
+                            child: IconButton(
                               onPressed: () {
                                 Navigator.pop(context);
                               },
                               icon: const Icon(
-                                CupertinoIcons.xmark,
+                                Icons.close,
                                 color: Colors.white,
                                 size: 28,
-                              )),
-                        ),
-                        Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            normalText(color: Colors.white, size: 24, text: "$seconds"),
-                            SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: CircularProgressIndicator(
-                                value: seconds / 60,
-                                valueColor: const AlwaysStoppedAnimation(Colors.white),
                               ),
                             ),
-                          ],
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            border: Border.all(color: lightgrey, width: 2),
                           ),
-                          child: TextButton.icon(
-                              onPressed: null,
-                              icon: const Icon(CupertinoIcons.heart_fill, color: Colors.white, size: 18),
-                              label: normalText(color: Colors.white, size: 14, text: "Like")),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Image.asset(ideas, width: 200),
-                    const SizedBox(height: 20),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: normalText(
-                            color: lightgrey,
-                            size: 18,
-                            text: "Question ${currentQuestionIndex + 1} of ${data.length}")),
-                    const SizedBox(height: 20),
-                    normalText(color: Colors.white, size: 20, text: data[currentQuestionIndex]["question"]),
-                    const SizedBox(height: 20),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: optionsList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        var answer = data[currentQuestionIndex]["correct_answer"];
-
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (answer.toString() == optionsList[index].toString()) {
-                                optionsColor[index] = Colors.green;
-                                points = points + 10;
-                              } else {
-                                optionsColor[index] = Colors.red;
-                              }
-
-                              if (currentQuestionIndex < data.length - 1) {
-                                Future.delayed(const Duration(seconds: 1), () {
-                                  gotoNextQuestion();
-                                });
-                              } else {
-                                timer!.cancel();
-                                //here you can do whatever you want with the results
-                              }
-                            });
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.only(bottom: 20),
+                          Stack(
                             alignment: Alignment.center,
-                            width: size.width - 100,
-                            padding: const EdgeInsets.all(16),
+                            children: [
+                              Text(
+                                "$seconds",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 80,
+                                height: 80,
+                                child: CircularProgressIndicator(
+                                  value: seconds / 30,
+                                  valueColor:
+                                      const AlwaysStoppedAnimation<Color>(
+                                          Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
                             decoration: BoxDecoration(
-                              color: optionsColor[index],
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: headingText(
-                              color: blue,
-                              size: 18,
-                              text: optionsList[index].toString(),
+                              borderRadius: BorderRadius.circular(16),
+                              border: Border.all(color: Colors.grey, width: 2),
                             ),
                           ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation(Colors.white),
-                ),
-              );
-            }
-          },
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      // Image.asset(
+                      //   'assets/images/ideas.png',
+                      //   width: 200,
+                      // ),
+                      const SizedBox(height: 20),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Question ${currentQuestionIndex + 1} of ${data.length}",
+                          style:
+                              const TextStyle(color: Colors.grey, fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        data[currentQuestionIndex]["question"],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      const SizedBox(height: 20),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: optionsList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          var answer =
+                              data[currentQuestionIndex]["correct_answer"];
+
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (answer.toString() ==
+                                    optionsList[index].toString()) {
+                                  optionsColor[index] = Colors.green;
+                                  // points = points + 10;
+                                } else {
+                                  optionsColor[index] = Colors.red;
+                                }
+
+                                if (currentQuestionIndex < data.length - 1) {
+                                  Future.delayed(const Duration(seconds: 1),
+                                      () {
+                                    gotoNextQuestion();
+                                  });
+                                } else {
+                                  timer!.cancel();
+                                  //here you can do whatever you want with the results
+                                }
+                              });
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              alignment: Alignment.center,
+                              width: size.width - 100,
+                              padding: const EdgeInsets.all(16),
+                              decoration: BoxDecoration(
+                                color: optionsColor[index],
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Text(
+                                optionsList[index].toString(),
+                                style: const TextStyle(
+                                    color: Colors.blue, fontSize: 18),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  ),
+                );
+              }
+            },
+          ),
         ),
-      )),
+      ),
     );
   }
 }
